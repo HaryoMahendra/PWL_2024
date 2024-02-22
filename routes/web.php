a2a44c9e91bd0194515788a0dac1a79031caed36
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-02-22 15:32:26 +0700

    Add new routes for hello, world, Selamat Datang, about, user, posts, and articles

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/hello', function () {
+ return 'Hello World';
+});
+
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/Selamat_Datang', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 2141762037 <br> Nama : Haryo Mahendra Jati';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Haryo Mahendra Jati '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', 
+    function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/posts/{post}/articles/{id} ', 
+    function ($postId, $id) {
+    return 'Pos ke-'.$postId." <br> Halaman Artikel dengan ID: ".$id;
 });
