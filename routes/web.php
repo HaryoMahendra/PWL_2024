19412adc0ab4b19faa287c5b9683cfc41caf2bea
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-02-25 13:58:20 +0700

    Update the display path web.php moving hello.blade.php in Blog

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ use App\Http\Controllers\PhotoController;
 //Route::resource('photos', PhotoController::class)->only(['index', 'show']);
 Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
 
+
 Route::get('/greeting', function () {
-    return view('hello', ['name' => 'Haryo Mahendra Jati']);
+    return view('blog.hello', ['name' => 'Haryo Mahendra Jati']);
 });
-   
