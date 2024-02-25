30d2ba0689a008ff6545b170369f12856f5a0a7f
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-02-25 13:14:59 +0700

    Refactor routes/web.php to use only index and show methods in PhotoController

--- routes/web.php
+++ routes/web.php
@@ -69,10 +69,6 @@ Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
 use App\Http\Controllers\PhotoController;
 //Route::resource('photos', PhotoController::class);
-//Route::resource('photos', PhotoController::class)->only([
-    //'index', 'show'
-   //]);
-Route::resource('photos', PhotoController::class)->except([
-   'create', 'store', 'update', 'destroy'
-   ]);
+//Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
    
